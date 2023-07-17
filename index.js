ad552bcdca5875d708610ccfc4c1594332ffb4b7
moproblems8 <mhasan1080@gmail.com>
2023-07-17 10:30:06 -0700

    footer done

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const toggleButton = document.getElementById('navbarToggle');
 const navBarLinks = document.querySelector('.navbar-links');
 
 toggleButton.addEventListener('click', function () {
-  return navBarLinks.classList.toggle('active');
+	return navBarLinks.classList.toggle('active');
 });
