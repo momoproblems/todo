4f43466b777edb0efec3770ba3983859cab47faa
moproblems8 <mhasan1080@gmail.com>
2023-07-18 11:00:24 -0700

    made sidebar

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const navBarLinks = document.querySelector('.navbar-links');
 toggleButton.addEventListener('click', function () {
 	return navBarLinks.classList.toggle('active');
 });
+
+// ! danger zone
+// ? question
+// * important
+// todo to do
