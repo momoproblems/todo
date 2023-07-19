222946e41a4cee6f95b9ef836ba1a0c804cee4a4
moproblems8 <mhasan1080@gmail.com>
2023-07-19 13:08:29 -0700

    updated all with webpack

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import '../node_modules/minireset.css/minireset.min.css';
+import '../static/style.scss';
+
+const toggleButton = document.getElementById('navbarToggle');
+const navBarLinks = document.querySelector('.navbar-links');
+
+toggleButton.addEventListener('click', function () {
+	return navBarLinks.classList.toggle('active');
+});
