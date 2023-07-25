223d4e1a81b7e77245e8da38816fd0439cc5f09b
moproblems8 <mhasan1080@gmail.com>
2023-07-24 22:56:43 -0700

    pause

--- src/index.js
+++ src/index.js
@@ -8,6 +8,49 @@ toggleButton.addEventListener('click', function () {
 	return navBarLinks.classList.toggle('active');
 });
 
+const taskForm = document.getElementById('taskForm');
+const taskTitle = document.getElementById('task');
+const taskDate = document.getElementById('dueDate');
+const taskDesc = document.getElementById('description');
+const taskPriority = document.getElementsByName('priority');
+
+taskForm.addEventListener('submit', function (e) {
+	console.log('form submitted');
+	console.log(taskTitle.value);
+	console.log(taskDate.value);
+	console.log(taskDesc.value);
+	console.log(taskPriority[0]);
+
+	e.preventDefault();
+	// handle error
+	let message = [];
+	if (taskTitle.value === '') {
+		message.push('Task title is required');
+	}
+	if (taskDate.value === '') {
+		message.push('Task due date is required');
+	}
+	if (taskDesc.value === '') {
+		message.push('Task description is required');
+	}
+	if (taskPriority.value === '') {
+		message.push('Task priority is required');
+	}
+	if (message.length > 0) {
+		alert(message.join(', '));
+		return;
+	}
+	// handle success
+
+	const task = {
+		title: taskTitle.value,
+		date: taskDate.value,
+		desc: taskDesc.value,
+		priority: taskPriority.value,
+	};
+	console.log(task);
+});
+
 // ! danger zone
 // ? question
 // * important
